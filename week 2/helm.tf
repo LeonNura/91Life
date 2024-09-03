e8303ab2b6e8aec459f123b4b61cb6aaa7afbc6e
LeonNura <45358960+LeonNura@users.noreply.github.com>
2024-09-03 12:48:38 +0200

    Add files via upload

---
+++ week 2/helm.tf
@@ -0,0 +1,50 @@
+data "google_container_cluster" "primary" {
+  name = "ninetyone-life-playground"
+  location = "europe-west1-b"
+}
+
+resource "null_resource" "helm_install" {
+  depends_on = [data.google_container_cluster.primary]
+
+  provisioner "local-exec" {
+    command = <<-EOF
+      # Download Helm
+      curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+      chmod 700 get_helm.sh
+      ./get_helm.sh
+
+      # Configure Helm to connect to the Kubernetes cluster
+      gcloud container clusters get-credentials ${data.google_container_cluster.primary.name} --zone ${data.google_container_cluster.primary.location} --project ${data.google_container_cluster.primary.project}
+
+      # Create a temporary configuration file
+      echo "api:
+  db:
+    host: my-postgresql.default.svc.cluster.local
+    name: mydb
+    port: 5432
+    username: admin
+    password: admin
+  migrations:
+    host: my-postgresql.default.svc.cluster.local
+    name: mydb
+    port: 5432
+    username: admin
+    password: admin
+  temporal:
+    url: temporal-url
+    defaultNamespace: temporal-namespace
+    defaultSyncJobQueue: temporal-queue
+
+worker:
+  temporal:
+    url: temporal-url
+    namespace: temporal-namespace
+    taskQueue: temporal-queue
+  redis:
+    url: redis-url" > config.yaml
+
+      # Install Neosync chart
+      helm upgrade --install neosync oci://ghcr.io/nucleuscloud/neosync/helm/neosync -f config.yaml
+    EOF
+  }
+}
---
+++ week 2/micro_instance_cluster.tf
@@ -0,0 +1,45 @@
+provider "google" {
+  project = "ninetyone-life-playground"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "ninetyone-life-playground"
+  location           = "europe-west1-b"
+  deletion_protection = false
+
+  node_pool {
+    name       = "default-pool"
+    node_count = 2
+
+    node_config {
+      machine_type = "g1-small"
+      disk_size_gb = 30
+
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+      ]
+    }
+
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
+  }
+}
+
+output "cluster_name" {
+  value = google_container_cluster.primary.name
+}
+
+output "cluster_endpoint" {
+  value = google_container_cluster.primary.endpoint
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "ninetyone-life-playground-terraform-state"
+    prefix = "terraform/state"
+  }
+}
